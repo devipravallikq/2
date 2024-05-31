14cb799a259cab9984ac0d924ebc914c24661d19
devipravallikq <164548355+devipravallikq@users.noreply.github.com>
2024-05-31 15:51:50 +0530

    Add files via upload

--- bubblesort.py
+++ bubblesort.py
@@ -12,38 +12,12 @@ def performBubbleSort(nums):
  
         fixThisIndex -= 1 
  
-def performSelectionSort(nums): 
-    n = len(nums)
-    fixThisIndex = n - 1 
-    while fixThisIndex > 0:
-        maxEleIndex = fixThisIndex
- 
- 
-        if maxEleIndex != fixThisIndex:
-            temp = nums[maxEleIndex] 
-            nums[maxEleIndex] = nums[fixThisIndex]
-            nums[fixThisIndex] = temp 
-        print(nums)
-        fixThisIndex -= 1 
- 
-def performInsertionSort(nums):
-    n = len(nums)
-    lastEleInSortedArr = 0 
-    for firstIndex in range(1, n):
-        temp = nums[firstIndex]
-        previous = lastEleInSortedArr 
- 
-        # [7, 9, 12, 34]
-        while previous >= 0 and nums[previous] > temp:
-            nums[previous + 1] = nums[previous]
-            previous -= 1 
-        nums[previous + 1] = temp 
-        lastEleInSortedArr += 1
+
  
 nums = [10, 8, 2, 14, 12, 7]
 #nums = list(map(int, input().split()))
 print("Before sorting:", nums)
  
-performInsertionSort(nums)
+
  
 print("After sorting:", nums)
